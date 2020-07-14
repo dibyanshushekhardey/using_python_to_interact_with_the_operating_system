49d94ad2b53e862e07bc40c1dadcc5b3027a99b9
Dibyanshu Shekhar Dey <66998557+dibyanshushekhardey@users.noreply.github.com>
2020-07-14 17:42:04 +0530

    Update code1.py

--- week_4/code1.py
+++ week_4/code1.py
@@ -16,3 +16,54 @@ while(cont.lower() == 'y'):
 
 print("Good Bye!")
 
+>>> import os
+>>> print("HOME: " + os.environ.get("HOME", ""))
+HOME: C:\Users\Hp
+>>> print("SHELL: " + os.environ.get("SHELL", ""))
+SHELL: 
+>>> print("FRUIT: " + os.environ.get("FRUIT", ""))
+FRUIT: 
+
+#Linux executable command
+#cat parameters.py
+import sys
+print(sys.argv)
+'''
+./paramers.py
+out:  ['./parameters.py']
+./parameters.py one two three
+out: ['./parameters.py', 'one', 'two', 'three']
+'''
+
+#wc command 
+'''
+wc variables.py
+out: 7 19 174 variables.py
+echo $?
+out: 0
+''' 
+#creating file
+import os
+import sys
+
+filename = sys.argv[1]
+
+if not os.path.exists(filename):
+    with open(filename, 'w') as f:
+        f.write("New file created\n")
+else:
+    print("Error, the file {] already exists".format(filename))
+    sys.exit(1)
+
+'''
+LINUX Command
+./create_file.py example
+echo $?
+out: 0
+cat example
+out: New File created
+./create_file.py example
+out: Error, the file example already exits
+echo $?
+out: 1
+'''
